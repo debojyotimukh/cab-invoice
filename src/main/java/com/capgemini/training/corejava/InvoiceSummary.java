bb060dff2fc3bf55be34040ec883920ece32811a
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-20 23:06:09 +0530

    created invoice summary class

---
+++ src/main/java/com/capgemini/training/corejava/InvoiceSummary.java
@@ -0,0 +1,34 @@
+package com.capgemini.training.corejava;
+
+public class InvoiceSummary {
+
+	private int noOfRides;
+	private double totalFare;
+	private double averageFare;
+
+	public InvoiceSummary(int noOfRides, double totalFare) {
+		this.noOfRides = noOfRides;
+		this.totalFare = totalFare;
+		this.averageFare=this.totalFare/this.noOfRides;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		InvoiceSummary other = (InvoiceSummary) obj;
+		if (Double.doubleToLongBits(averageFare) != Double.doubleToLongBits(other.averageFare))
+			return false;
+		if (noOfRides != other.noOfRides)
+			return false;
+		if (Double.doubleToLongBits(totalFare) != Double.doubleToLongBits(other.totalFare))
+			return false;
+		return true;
+	}
+
+
+}
