0953b8732ac2b5012cc5f3fe8b149a455f97d81e
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-20 22:26:01 +0530

    test case: for checking minimum fare condition

--- src/test/java/com/capgemini/training/corejava/InvoiceServiceTest.java
+++ src/test/java/com/capgemini/training/corejava/InvoiceServiceTest.java
@@ -7,11 +7,20 @@ import org.junit.Assert;
 public class InvoiceServiceTest {
 
     @Test
-    public void givenDistanceAmsTime_ShouldReturnTotalFare() {
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
         InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
-        double fare = invoiceGenerator.calculateFare(distance,time);
-        Assert.assertEquals(25,fare,0.0);
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(25, fare, 0.0);
+    }
+
+    @Test
+    public void givenLessDistanceOrTime_shouldReturnMinFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
     }
 }
