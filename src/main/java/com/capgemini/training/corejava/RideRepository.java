3218353c01570f9f42cc226c78931c7612ed7f9a
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-21 01:43:43 +0530

    refactor: merge new rides with existing ones

--- src/main/java/com/capgemini/training/corejava/RideRepository.java
+++ src/main/java/com/capgemini/training/corejava/RideRepository.java
@@ -1,24 +1,27 @@
 package com.capgemini.training.corejava;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class RideRepository {
-    private Map<String, List<Ride>> userRides;
+    private final Map<String, ArrayList<Ride>> userRides;
 
     public RideRepository() {
         this.userRides = new HashMap<>();
     }
 
-    public void addRides(String userId, Ride[] rides) {
-        List<Ride> existingRides = this.userRides.get(userId);
-        if (existingRides == null)
-            this.userRides.put(userId, Arrays.asList(rides));        
+    public void addRides(final String userId, final Ride[] rides) {
+        final ArrayList<Ride> rideList = new ArrayList<>();
+        Collections.addAll(rideList, rides);
+        userRides.merge(userId, rideList, (vo, vn) -> {
+            vo.addAll(vn);
+            return vo;
+        });
     }
 
-    public Ride[] getRides(String userId) {
+    public Ride[] getRides(final String userId) {
         return this.userRides.get(userId).toArray(new Ride[0]);
     }
 
